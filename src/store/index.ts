f511a12c2670223bae8aef0080866058dd6cfe04
artnomic <arthurpe44@gmail.com>
2024-12-04 18:26:10 -0300

    feat/exercise

--- src/store/index.ts
+++ src/store/index.ts
@@ -10,6 +10,7 @@ export const store = configureStore({
     carrinho: carrinhoReducer,
     favorito: favoritoReducer,
     [api.reducerPath]: api.reducer
+    
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware)
